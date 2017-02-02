f462d940e4a61c440b5c112a853c16a541a283bb
Josiah <josi@fisler.ch>
2017-02-02 23:31:23 +0100

    format

--- firstProject/src/firstProject/PrimZahlen.java
+++ firstProject/src/firstProject/PrimZahlen.java
@@ -5,47 +5,41 @@ import java.util.Scanner;
 public class PrimZahlen {
 
 	public static void main(String[] args) {
-		// ich möchte einen aloritmus entwerfen der eine mir Sagen kann ob eine Zahl prim ist oder nicht
-	int eingeleseneZahl = einlesen();
-	//System.out.println(eingeleseneZahl);
-	boolean isPrime = true;
-	for (int teiler = 2; teiler < eingeleseneZahl && isPrime == true && eingeleseneZahl > 0; teiler++){
-		int resultat = eingeleseneZahl % teiler;
-		if(resultat == 0 ){
-			isPrime = false;
+		// ich möchte einen aloritmus entwerfen der eine mir Sagen kann ob eine
+		// Zahl prim ist oder nicht
+		int eingeleseneZahl = einlesen();
+		// System.out.println(eingeleseneZahl);
+		boolean isPrime = true;
+		for (int teiler = 2; teiler < eingeleseneZahl && isPrime == true
+				&& eingeleseneZahl > 0; teiler++) {
+			int resultat = eingeleseneZahl % teiler;
+			if (resultat == 0) {
+				isPrime = false;
+			}
+
 		}
-		
+		if (isPrime == true && eingeleseneZahl > 0) {
+			System.out.println("ist eine Primzahl");
+		} else if (isPrime == false && eingeleseneZahl > 0) {
+			System.out.println("ist keine Primzahl");
+		} else if (isPrime == true && eingeleseneZahl == 0) {
+			// hier darf nichts passieren
+		} else if (isPrime == true && eingeleseneZahl < 0) {
+			System.out.println("das ist eine negative Zahl");
 		}
-	if (isPrime == true && eingeleseneZahl > 0){
-		System.out.println("ist eine Primzahl");
-	} 
-	else if (isPrime == false && eingeleseneZahl > 0){
-		System.out.println("ist keine Primzahl");
 	}
-	else if (isPrime == true && eingeleseneZahl == 0){
-		//hier darf nichts passieren 
+
+	static int einlesen() {
+		System.out.println("Bitte geben sie eine Zahl ein");
+		Scanner myScanner = null;
+		int eingabe = 0;
+		try {
+			myScanner = new Scanner(System.in);
+			eingabe = myScanner.nextInt();
+		} catch (Exception InputMismatchException) {
+			System.out.println("das ist keine Zahl");
+		}
+		myScanner.close();
+		return eingabe;
 	}
-	else if (isPrime == true && eingeleseneZahl < 0){
-		System.out.println("das ist eine negative Zahl");
-	}
-	}
-	
-	
-	
-		
-	
-	
-static int einlesen(){
-	System.out.println("Bitte geben sie eine Zahl ein");
-	Scanner myScanner = null;
-	int eingabe = 0;
-	try {
-		myScanner = new Scanner(System.in);
-		eingabe = myScanner.nextInt();
-	} catch (Exception InputMismatchException) {
-		System.out.println("das ist keine Zahl");
-	}
-	myScanner.close();
-	return eingabe;
-}
 }
--- firstProject/src/firstProject/ScannerException.java
+++ firstProject/src/firstProject/ScannerException.java
@@ -14,18 +14,23 @@ public class ScannerException {
 		int enter = myScanner.nextInt();
 		String enter2 = myScanner2.nextLine();
 		String enter3 = myScanner3.nextLine();
-		System.out.println("You may now Selec what you want to do with this celebreties one after another");
-		String[] Options = {"Shoot", "Marry", "Bang"};
-		 JOptionPane.showOptionDialog(null, "Would you like to _____ " + enter, "First Celebity", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, Options, null);
-		JOptionPane.showOptionDialog(null, "Would you like to _____ " + enter2, "Second Celebity", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, Options, null);
-		JOptionPane.showOptionDialog(null, "Would you like to _____ " + enter3, "Third Celebity", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, Options, null);
+		System.out
+				.println("You may now Selec what you want to do with this celebreties one after another");
+		String[] Options = { "Shoot", "Marry", "Bang" };
+		JOptionPane.showOptionDialog(null, "Would you like to _____ " + enter,
+				"First Celebity", JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, Options, null);
+		JOptionPane.showOptionDialog(null, "Would you like to _____ " + enter2,
+				"Second Celebity", JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, Options, null);
+		JOptionPane.showOptionDialog(null, "Would you like to _____ " + enter3,
+				"Third Celebity", JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE, null, Options, null);
 
 		myScanner.close();
 		myScanner2.close();
 		myScanner3.close();
-		
-		
-	
+
 	}
-	
+
 }
